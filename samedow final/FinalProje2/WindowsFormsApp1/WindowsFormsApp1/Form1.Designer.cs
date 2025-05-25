e517b71a96fcb589a0e522b6d79fc213d65c989e
samettakgül <sametakgul154@gmail.com>
2025-05-25 22:04:41 +0300

    doviz_cevirici
    döviz

---
+++ samedow final/FinalProje2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Miktar (TRY)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Para Birimi :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sonuç :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "USD (Amerikan Doları)",
+            "EUR (Euro)",
+            "GBP (İngiliz Sterlini)",
+            "CHF (İsviçre Frangı)",
+            "JPY (Japon Yeni)",
+            "CAD (Kanada Doları)",
+            "AUD (Avustralya Doları)",
+            "CNY (Çin Yuanı)",
+            "SEK (İsveç Kronu)",
+            "NOK (Norveç Kronu)"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 137);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(319, 36);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 34);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 40);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Dönüştür";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(189, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Kapat";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 327);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kur Takibi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ samedow final/FinalProje2/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Double TLpara = 0;
+        double USD = 0.02564;
+        double EUR = 0.02257;
+        double GBP = 0.01894;
+        double CHF = 0.02106;
+        double JPY = 3.65583;
+        double CAD = 0.03522;
+        double NOK = 0.18543;
+        double AUD = 0.03948;
+        double CNY = 0.18543;
+        double SEK = 0.24446;
+        double sonuc = 0;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TLpara = Convert.ToDouble(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Lütfen geçerli bir miktar giriniz." , "Hata!", MessageBoxButtons.OK , MessageBoxIcon.Error);
+            }
+
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Lütfen bir para birimi seçiniz.", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if ((comboBox1.Text != "USD(Amerikan Doları)") && (comboBox1.Text != "EUR (Euro)") && (comboBox1.Text != "GBP (İngiliz Sterlini)") && (comboBox1.Text != "CHF (İsviçre Frangı)") && (comboBox1.Text != "JPY (Japon Yeni") && (comboBox1.Text == "CAD (Kanada Doları)") && (comboBox1.Text == "NOK (Norveç Kronu)") && (comboBox1.Text == "AUD (Avusturya Doları)") &&   (comboBox1.Text == "CNY(Çin Yuanı)") && (comboBox1.Text == "SEK (İsveç Kronu)"))
+            {
+                MessageBox.Show("Lütfen geçerli bir para birimi seçiniz.", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (comboBox1.Text == "USD (Amerikan Doları)")
+            {
+                sonuc = TLpara * USD;
+                sonuc.ToString();
+                label3.Text = sonuc.ToString() + " " + comboBox1.Text;
+            }
+            else if (comboBox1.Text == "EUR (Euro)")
+            {
+                sonuc = TLpara * EUR;
+                sonuc.ToString();
+                label3.Text = sonuc.ToString() + " " + comboBox1.Text;
+            }
+            else if (comboBox1.Text == "CHF (İsviçre Frangı)")
+            {
+                sonuc = TLpara * CHF;
+                sonuc.ToString();
+                label3.Text = sonuc.ToString() + " " + comboBox1.Text;
+            }
+            else if (comboBox1.Text == "JPY (Japon Yeni)")
+            {
+                sonuc = TLpara * JPY;
+                sonuc.ToString();
+                label3.Text = sonuc.ToString() + " " + comboBox1.Text;
+            }
+            else if (comboBox1.Text == "CAD (Kanada Doları)")
+            {
+                sonuc = TLpara * CAD;
+                sonuc.ToString();
+                label3.Text = sonuc.ToString() + " " + comboBox1.Text;
+            }
+            else if (comboBox1.Text == "AUD (Avustralya Doları)")
+            {
+                sonuc = TLpara * AUD;
+                sonuc.ToString();
+                label3.Text = sonuc.ToString() + " " + comboBox1.Text;
+            }
+            else if (comboBox1.Text == "CNY (Çin Yuanı)")
+            {
+                sonuc = TLpara * CNY;
+                sonuc.ToString();
+                label3.Text = sonuc.ToString() + " " + comboBox1.Text;
+            }
+            else if (comboBox1.Text == "SEK (İsveç Kronu)")
+            {
+                sonuc = TLpara * SEK;
+                sonuc.ToString();
+                label3.Text = sonuc.ToString() + " " + comboBox1.Text;
+            }
+            else if (comboBox1.Text == "NOK (Norveç Kronu)")
+            {
+                sonuc = TLpara * NOK;
+                sonuc.ToString();
+                label3.Text = sonuc.ToString() + " " + comboBox1.Text;
+            }
+            else if (comboBox1.Text == "GBP (İngiliz Sterlini)")
+            {
+                sonuc = TLpara * GBP;
+                sonuc.ToString();
+                label3.Text = sonuc.ToString() + " " + comboBox1.Text;
+            }
+            
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
